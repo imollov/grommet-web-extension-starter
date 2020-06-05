d491b86a9fc468f488fcdfd80d54819b1d1eb207
imollov <ivelin.mollov@hpe.com>
2020-06-05 15:56:00 +0300

    Set webpack watch mode in dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
+  watch: true,
   devtool: 'inline-source-map',
   plugins: [
     new ChromeExtensionReloader({
